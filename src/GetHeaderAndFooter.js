8e42908ebc14deeaa8cf5f20ef8dc4bb04ba8d47
Kamp Duong <zendeli.dev@gmail.com>
2021-03-29 00:57:13 -0400

    Added Cape Coral Location

--- src/GetHeaderAndFooter.js
+++ src/GetHeaderAndFooter.js
@@ -61,7 +61,7 @@ CreateHeader = () => {
         `<header class="sticky-top">
             <nav class="navbar navbar-expand-md navbar-light bg-light">
                 <a class="navbar-brand ml-1" href="index.html">
-                    <img src="assets/ZenDeli.svg" width="96px">
+                    <img id="logo" src="assets/ZenDeli.svg" width="96px">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -74,6 +74,15 @@ CreateHeader = () => {
                         <!-- <a class="nav-item nav-link disabled${isOnAboutPage ? active : ""}" href="aboutus.html">About us</a> -->
                     </div>
                     <div class="navbar-nav navbar-right d-flex flex-row">
+                        <span class="navbar-text container d-flex flex-column mx-2">
+                            <label class="row justify-content-center phone-label">Cape Coral Location:</label>
+                            <div class="row justify-content-center">
+                                Coming
+                            </div>
+                            <label class="row justify-content-center">
+                                Soon
+                            </label>
+                        </span>
                         <span class="navbar-text container d-flex flex-column mx-2">
                             <label class="row justify-content-center phone-label">South Fort Myers Location:</label>
                             <div class="row justify-content-center">
@@ -102,7 +111,7 @@ CreateHeader = () => {
 
 CreateFooter = () => {
     document.body.innerHTML +=
-        `<footer class="footer">
+        `<footer class="footer bg-dark text-warning">
             <div class="row bg-secondary m-0 p-4 text-light">
                 <div class="col col-12 col-lg-4 py-3 d-flex justify-content-center">
                     <div class="text-center col justify-content-between">
@@ -111,25 +120,39 @@ CreateFooter = () => {
                         </h5>
                         <div>
                             <!-- Main Campus -->
-                            <div class="font-weight-bold text-warning ">ZEN Delicatessen</div>
-                            <div class="mt-2 font-weight-bold">Main Location</div>
-                            <div>15880 San Carlos Blvd. #135</div>
-                            <div>Fort Myers, FL 33908</div>
+                            <div class="mt-2 font-weight-bold text-warning">Main Location</div>
+                            <p class="text-white mt-2">
+                                15880 San Carlos Blvd. #135<br />
+                                Fort Myers, FL 33908
+                            </p>
                             <div class="mt-2">
                                 <a class="text-warning" href="tel:239-362-3049">
                                     <i class="fas fa-phone-alt mr-2"></i>(239) 362 - 3049
                                 </a> 
                             </div>
                         </div>
-                        <div>
+                        <div class="mt-2">
                             <!-- Flea Market Stall -->
-                            <div class="mt-4 font-weight-bold">Fleamasters Flea Market</div>
-                            <div>4135 Dr. Martin Luther King Jr. Blvd.</div>
-                            <div>Fort Myers, FL 33916</div>
+                            <div class="mt-4 font-weight-bold text-warning">Fleamasters Flea Market</div>
+                            <p class="text-white mt-2">
+                                4135 Dr. Martin Luther King Jr. Blvd.<br />
+                                Fort Myers, FL 33916
+                            </p>
                             <div class="mt-2">
                                 <a class="text-warning" href="tel:239-313-5272">
                                     <i class="fas fa-phone-alt mr-2"></i>(239) 313 - 5272
                                 </a> 
+                            </div>
+                        </div>
+                        <div class="mt-2">
+                            <!-- Cape Coral Campus -->
+                            <div class="mt-4 font-weight-bold text-warning">Cape Coral Location</div>
+                            <p class="text-white mt-2">
+                                1847 NE Pine Island Rd. #650<br />
+                                Cape Coral, FL 33909
+                            </p>
+                            <div class="mt-2 text-warning">
+                                Coming Soon
                             </div>
                         </div>
                     </div>
@@ -141,7 +164,7 @@ CreateFooter = () => {
                             <i class="far fa-clock"></i>&nbsp;HOURS
                         </h5>
                         <div>
-                            <div class="mt-4 font-weight-bold">Main Location</div>
+                            <div class="mt-4 font-weight-bold text-warning">Main Location</div>
                             <div class="row">
                                 <span class="col col-4 text-left">Monday</span>
                                 <span class="col col-8 text-right">11 AM - 8 PM</span>
@@ -172,7 +195,7 @@ CreateFooter = () => {
                             </div>
                         </div>
                         <div>
-                            <div class="mt-4 font-weight-bold">Fleamasters Flea Market</div>
+                            <div class="mt-4 font-weight-bold text-warning">Fleamasters Flea Market</div>
                             <div class="row">
                                 <span class="col col-4 text-left">Monday</span>
                                 <span class="col col-8 text-right">Closed</span>
@@ -235,27 +258,3 @@ CreateFooter = () => {
 
 CreateHeader();
 CreateFooter();
-
-/* Header Phone Number
-                        <span class="navbar-text container d-flex flex-column mx-2">
-                            <label class="row justify-content-center phone-label">Fleamasters FleaMarket:</label>
-                            <div class="row justify-content-center">
-                                <a href="tel:239-362-3049">
-                                    <i class="fas fa-phone-alt"></i>&nbsp;(239) 362 - 3049
-                                </a>
-                            </div>                            
-                        </span>
-*/
-
-
-/* Footer Location
-                        <!-- Cape Coral Campus -->
-                        <!-- <div class="mt-4 font-weight-bold">Cape Coral Location</div>
-                        <div>41847 NE Pine Island Rd. #650</div>
-                        <div>Cape Coral, FL 33909</div>
-                        <div class="mt-2">
-                            <a class="text-warning" href="tel:239-313-5272">
-                                <i class="fas fa-phone-alt mr-2"></i>(239) 313 - 5272
-                            </a> 
-                        </div> -->
-*/
